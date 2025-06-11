12456419a9ad5ce7c276c6342d18fe68eddb57b6
chao0425 <chao042025@outlook.com>
2025-06-11 18:29:52 +0800

    尝试一下vscode里面这个提交是做什么的

--- coreJava/HelloJava.java
+++ coreJava/HelloJava.java
@@ -1,5 +1,15 @@
+import org.junit.Test;
+
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("hello java");
+        System.out.println("what??? \n fuck");
+    }
+
+    @Test
+    public void run() {
+        System.out.println("fuck");
+
+        System.out.println("JUnit 测试结果在调试控制台打印");
     }
 }
