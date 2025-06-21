0eaace3a313081d1d0213299b9925876ada56deb
chao0425 <chao042025@outlook.com>
2025-06-21 23:20:25 +0800

    corejava1

--- coreJava/coreJava1/chapter_3/IOAndBlock.java
+++ coreJava/coreJava1/chapter_3/IOAndBlock.java
@@ -1,8 +1,13 @@
 import java.io.Console;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 import java.util.*;
 
 public class IOAndBlock {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+
     // 对于输出没啥好说的，我们之前的println可以在控制台输出，我们接下来看看控制台输入
 	
 	Scanner systemIn = new Scanner(System.in);
@@ -36,6 +41,34 @@ public class IOAndBlock {
 	String message = String.format("this is similar to printf\n %d but not print. %f this is store", 666, 33.33);
 
 	System.out.println(message);
+
+	// 这个Path.of("文件路径")其中文件路径是绝对路径和相对路径都可以,如果是绝对路径自然就好了，如果仅仅是像这样写，那么用vscode打开的时候要打开它所在的文件夹，即CHAPTER_3。具体路径情况61页有解释
+	Scanner scanner = new Scanner(Path.of("file.txt"), StandardCharsets.UTF_8);
+
+	while(scanner.hasNextLine()){
+		System.out.println(scanner.nextLine());
+	}
+
+	// 这个文件路径也是与在哪里打开有关，最后别忘了close.这个append不是追加，而是擦除后写入
+	PrintWriter out = new PrintWriter("out.txt",StandardCharsets.UTF_8);
+
+	out.append("this is write to a txt\n, and similar to Scanner by file.\n and don\'t forget close");
+
+	out.close();
+	scanner.close();
+	systemIn.close();
+
+	// 对于输入输出，记得抛出异常
+	// Scanner实在util包，PrintWriter再io包，Path在nio.file包
+
+	int number = 3;
+	// int number = 5;
+	System.out.println(number);
+	if(true) {
+		// int number = 5;
+	}
+	// 在一个块内，不要重复命名，在一个块的子块内，也不可以重复（与外层快的重复）命名
+	System.out.println(number);
     }
 
 }
