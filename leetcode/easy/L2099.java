8e9a798e204789f72f098e39ff9103da0a91fc59
chao0425 <chao042025@outlook.com>
2025-06-28 22:25:34 +0800

    leetcode

---
+++ leetcode/easy/L2099.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+class L2099 {
+    // 感觉是用堆来实现是更合理的，但是简单题简单做吧，刚刚由于电的问题吓死了。
+    // TODO
+    public int[] maxSubsequence(int[] nums, int k) {
+        
+    }
+}
