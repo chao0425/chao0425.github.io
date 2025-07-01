d190157d953c478d8bb2e488a19ded9c2c021d7f
chao0425 <chao042025@outlook.com>
2025-07-01 23:07:39 +0800

    解决github的换行与vscode看着不一样的问题

---
+++ leetcode/middle/L1498.java
@@ -0,0 +1,11 @@
+class L1498 {
+    // 感觉是滑动窗口
+    // 看了一眼，排序后再搞就好多了，只需要知道有多少个子序列就好，与顺序无关
+    public int numSubseq(int[] nums, int target) {
+        int left = 0;
+        int ans = 0;
+        for(int right = 0; right < nums.length; right++) {
+
+        }
+    }
+}
