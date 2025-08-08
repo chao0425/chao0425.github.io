6a71a6ef1634a0511f5ea4f06f0fb4e6dd2c8922
chao0425 <chao042025@outlook.com>
2025-08-08 23:28:26 +0800

    dataType&var

---
+++ html&css&javascript/javascript/1.0dataType&var/dataType&var.js
@@ -0,0 +1,27 @@
+var str = "String"
+console.log(str)
+console.log("type is : " + typeof(str))
+
+str = "we can change content"
+console.log(str)
+console.log("type is : " + typeof(str))
+
+str = 666
+console.log(str)
+console.log("type is : " + typeof(str))
+
+str = false
+console.log(str + " type is : " + typeof(str))
+
+var str = "oh god"
+console.log(str)
+
+str = new Object()
+console.log(str)
+console.log("type is : " + typeof(str))
+
+console.log(typeof("String"))
+console.log(typeof "String")
+
+var nothing
+console.log(nothing)
