737b8806c4d77f72c845a348c0b850ae9993229f
chao0425 <chao042025@outlook.com>
2025-08-09 22:38:31 +0800

    operator

---
+++ html&css&javascript/javascript/2.0operator/operator.js
@@ -0,0 +1,28 @@
+console.log(10/4);
+console.log(10/2);
+console.log(10/0);
+console.log(10%0);
+
+console.log("----------------------------------------");
+
+var i = 10;
+console.log(i /= 4);
+console.log(i /= 2);
+console.log(i /=0);
+console.log(i %= 0);
+
+console.log("----------------------------------------");
+
+console.log(1 == 1);
+console.log(1 == "1");
+console.log("1" == "1");
+console.log(1 == true);
+console.log(2 == true);
+
+console.log("----------------------------------------");
+
+console.log(1 === 1);
+console.log(1 === "1");
+console.log("1" === "1");
+console.log(1 === true);
+console.log(2 === true);
