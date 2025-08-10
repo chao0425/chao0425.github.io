c1d5a9aa37e0e0bad8244e92f969d8b72e9ce902
chao0425 <chao042025@outlook.com>
2025-08-10 13:54:44 +0800

    object

---
+++ html&css&javascript/javascript/4.0object/object.js
@@ -0,0 +1,21 @@
+var people = new Object()
+
+people.name = "Bob";
+people.age = 18;
+
+people.sayMe = function(hobby) {
+    console.log("My name is : " + this.name + " and my age is : " + this.age + " then my hobby is : " + hobby)
+}
+
+people.sayMe("nothing")
+
+var people = {
+    name : "Alice",
+    "age" : 28,
+    "job" : "teacher",
+    sayMe : function(hobby) {
+        console.log("Hello my name is " + this.name + " and my age is " + this.age + " my job is " + this.job + "my hobby is " + hobby)
+    }
+}
+
+people.sayMe("nothing")
