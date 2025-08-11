0dc99a56613c5a78f97b1a8c677510a6eabee6c0
chao0425 <chao042025@outlook.com>
2025-08-11 21:28:42 +0800

    event key&mouse

---
+++ html&css&javascript/javascript/7.0event/event.js
@@ -0,0 +1,35 @@
+function hello() {
+    alert("Hello!!!")
+}
+
+function other() {
+    alert("嘿嘿，可以绑定不止一个函数")
+}
+
+function double() {
+    alert("这个是双击绑定的效果")
+}
+
+function over() {
+    console.log("鼠标在这个框内悬浮")
+}
+
+function enter() {
+    console.log("鼠标进入这个框了，看起来与悬浮差不多")
+}
+
+function move() {
+    console.log("鼠标在框内乱动什么\n鼠标在框内乱动什么\n鼠标在框内乱动什么\n鼠标在框内乱动什么\n鼠标在框内乱动什么\n鼠标在框内乱动什么")
+}
+
+function leave() {
+    console.log("离开了框框")
+}
+
+function keyDown() {
+    console.log("down")
+}
+
+function keyUp() {
+    console.log("松开了键盘")
+}
